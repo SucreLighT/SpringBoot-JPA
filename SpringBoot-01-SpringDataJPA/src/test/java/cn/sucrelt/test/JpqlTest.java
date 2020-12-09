c22e84fb6f1879bf3f6552fb145ea725a8065167
tangtangsama <sucre@nuaa.edu.cn>
2020-12-09 16:10:58 +0800

    SpringDataJPA使用JPQL语句查询

---
+++ SpringBoot-01-SpringDataJPA/src/test/java/cn/sucrelt/test/JpqlTest.java
@@ -0,0 +1,37 @@
+package cn.sucrelt.test;
+
+
+import cn.sucrelt.dao.CustomerDao;
+import cn.sucrelt.domain.Customer;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class) //声明spring提供的单元测试环境
+@ContextConfiguration(locations = "classpath:applicationContext.xml")//指定spring容器的配置信息
+public class JpqlTest {
+    @Autowired
+    private CustomerDao customerDao;
+
+    @Test
+    public void testFindCustNameAndId() {
+        Customer customer = customerDao.findCustNameAndId(1l, "王二");
+        System.out.println(customer);
+    }
+
+    @Test
+    @Transactional //添加事务的支持
+    @Rollback(value = false)
+    public void testUpdateCustomer() {
+        customerDao.updateCustomer(4l, "麻子");
+    }
+
+}
